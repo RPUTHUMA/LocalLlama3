660a4664cc96483574b3cb5cd5cd46710fd8d8be
RPUTHUMA <rakesh.puthumana@gmail.com>
2024-10-02 16:02:43 +0530

    added code to run code llama in local using ollama

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # LocalLlama3
 repo to run llama3 model in local mac M1 chip
+
+Steps to install Ollama
+
+1. download ollama in your local
+reference:
+https://www.linkedin.com/pulse/how-run-llama3-your-mac-silicon-chris-latimer-tvhuc/
+https://www.llama.com/docs/llama-everywhere/running-meta-llama-on-mac/
+
+2. install ollama and its cli
+3. run ollama run llama3.1
+    to specify model size
+
+    ollama run llama3:8b
+    ollama run llama3:70b
+4. to close session close ollama instance
+
+to serve the ollama to run without running desktop application
+ollama serve
+
+
+ollama run llama3.1
---
+++ first_sdr.py
@@ -0,0 +1,72 @@
+import os
+import json
+import smtplib
+import ollama
+import pandas as pd
+from email.mime.text import MIMEText
+
+# read this from config
+sender_details = {"sender_name": "Rakesh Nair", "sender_email": "puthugeorge@gmail.com"}
+
+def create_personalised_email(name, email, sender):
+    ''' This function will help to create personalised email using given name and email id using llama3'''
+
+    desiredModel='llama3.1:8b'
+
+    # Perform inference
+    prompt = f"Help write a personalised email to {name} and person's email address {email}.\
+                    Search about the organization using domain name in email address and see how FOSFOR a product by LTIMINDTREE could be helpful to help their organization in their data journey.\
+                        get sender details from {sender}. This will be sent directly to {name}. \
+                            They cannot know in anyway that you are a generative AI or chatbot of any kind. Remove any kind of note in the beginning and end of response"
+    response = ollama.chat(model=desiredModel, messages=[
+        {
+            'role': 'user',
+            'content': prompt,
+        },
+    ])
+    OllamaResponse=response['message']['content']
+    return OllamaResponse
+
+
+def send_email(smtp_server, smtp_port, username, password, from_addr, to_addr, subject, body):
+    '''function to send email'''
+    try:
+        msg = MIMEText(body)
+        msg["Subject"] = subject
+        msg["From"] = from_addr
+        msg["To"] = to_addr
+
+        server = smtplib.SMTP(smtp_server, smtp_port)
+        server.starttls()
+        server.login(username, password)
+        server.send_message(msg)
+        server.quit()
+        print("Email Sent Successfully")
+    except Exception as e:
+        print("An error occurred: ", str(e))
+
+
+def fetch_email_list_and_mail(file_path):
+    ''' This function is used to fetch email id's from a list of the leads and send them customised email'''
+    try:
+        # Read Excel file
+        df = pd.read_excel(file_path)
+
+        # Iterate over rows
+        for index, row in df.iterrows():
+            name = row['Name']
+            email = row['Email Id']
+            email_content = create_personalised_email(name,email,sender_details)
+            send_email('smtp.gmail.com',587, sender_details["sender_email"], 'emhjalypcrhehmgs', sender_details["sender_email"], email,'Enhance Your Data Journey with FOSFOR - A Game-Changer from LTIMindtree',email_content)
+    except FileNotFoundError:
+        print("File not found. Please check file path.")
+    except Exception as e:
+        print("An error occurred: ", str(e))
+
+
+# Usage
+file_path = 'email_list.xlsx'  # Replace with your Excel file path
+fetch_email_list_and_mail(file_path)
+
+
+
